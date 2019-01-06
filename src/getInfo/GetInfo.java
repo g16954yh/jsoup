b54a96118981de4292fb12854ad2c4613323f8e0
yazawaharuka <yazawaharuka@192.168.10.4>
2019-01-06 23:08:23 +0900

    URLの取得

--- src/getInfo/GetInfo.java
+++ src/getInfo/GetInfo.java
@@ -16,11 +16,17 @@ public class GetInfo {
 			//ファイルの書き込み
 			fw = new FileWriter("data",true);
 			Document document = Jsoup.connect("https://travel.yahoo.co.jp/dhotel/shisetsu/HT10024471/review?facet_flg=1&dcbpt=1").get();
-			Elements elements = document.select(".navi li p span");
+			Elements url = document.select(".ttl h2 a");
+			for(Element element : url) {
+				String data = element.attr("href");
+				fw.write(data+"\n");
+			}
+			/**Elements elements = document.select(".navi li p span");
 			for(Element element : elements) {
 				String data = element.text();
 				fw.write(data+",");
 			}
+			*/
 			fw.flush();
 		} catch (IOException e) {
 			System.out.println("ファイル書き込みエラーです");
